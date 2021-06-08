621ccf49969c2927068fc3555170199653f368f2
villysiu <villysiu@gmail.com>
2021-06-07 19:29:41 -0700

    commit tree c9f5ca13e5aac97a3914614cc55a31845720ef81

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 
 // Write your code here
+
+const num1 = 31
+const num2 = 2
+const multiply = num1*num2
+const random = Math.round(Math.random()*100)
+const mod = 16 % 6
+const max = Math.max(1,5,20)
